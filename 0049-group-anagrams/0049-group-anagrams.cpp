e82cdf2d18ff1784c211febfa855ab6c3a2f8d5d
idkmynamereally <97702836+idkmynamereally@users.noreply.github.com>
2023-09-14 21:47:05 +0530

    Time: 48 ms (19.46%), Space: 31.2 MB (5.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,23 +3,20 @@ class Solution
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs)
     {
-        map<map<char, int>, vector<string>> groups{};     //All groups <anagramMap, Strings    
+        map<vector<int>, vector<string>> groups{};     //All groups <anagramMap, Strings    
         string str{};
         for (int i = 0; i < strs.size(); i++)
         {
-            map<char, int> anagramMap{};
+            vector<int> anagramMap(26);
             str = strs[i];
             for (char c : str)
             {
-                if (anagramMap.contains(c))
-                    anagramMap[c]++;
-                else
-                    anagramMap[c] = 1;
+                anagramMap[c - 97]++;
             }
             groups[anagramMap].push_back(strs[i]);
         }
         vector<vector<string>> ret;
-        for (pair<map<char, int>, vector<string>> v : groups)
+        for (auto v : groups)
         {
             ret.push_back(v.second);
         }
