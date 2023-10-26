d7827f7d3fe177453c734db6be207456324d52e5
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2023-10-27 00:03:29 +0530

    Time: 4 ms (80.56%), Space: 7.5 MB (65.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,34 +2,30 @@ class Solution {
 public:
     bool isPalindrome(std::string s) 
     {
-        std::string s2;
-        for (int i = 0; i < s.length(); i++)
+        int l = 0;
+        int r = s.length() - 1;
+        while(l < r)
         {
-            char c = s[i];
-            if (c >= 97 && c <= 122)    //lowercase letters
+            char cl = s[l];
+            char cr = s[r];
+            if (!isalnum(cl))
             {
-                s2.push_back(c);
+                l++;
                 continue;
             }
-            if (c >= 48 && c <= 57)     //numerics
+            if (!isalnum(cr))
             {
-                s2.push_back(c);
+                r--;
                 continue;
             }
-            if (c >= 65 && c <= 90)     //uppercase to lowercase
-            {
-                s2.push_back(c + 32);
-                continue;
-            }
-            else                        //remove non alphanumerics
-            {
-                continue;
-            }
-        }
-        for (int l = 0, r = s2.length() - 1; l < r; l++, r--)
-        {
-            if (s2[l] != s2[r])
+            if (cl >= 65 && cl <= 90)
+                cl += 32;
+            if (cr >= 65 && cr <= 90)
+                cr += 32;
+            if (cl != cr)
                 return false;
+            l++;
+            r--;
         }
         return true;
     }
