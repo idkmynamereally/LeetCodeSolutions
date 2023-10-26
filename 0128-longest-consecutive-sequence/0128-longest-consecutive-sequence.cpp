f2a3e5fa1e7728f209e78f2b164e02ed056ae1ff
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2023-10-26 22:59:03 +0530

    Time: 1128 ms (10.63%), Space: 71 MB (16.58%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    //Approach : All values that have a left neighbor cannot be the start of
+    // a sequence of consecutive numbers. so we go to every number if it has a
+    // left neighbor we skip if not we see how long of a sequence it has
+    int longestConsecutive(std::vector<int>& nums) 
+    {
+        int max = 0;
+        std::unordered_set<int> allValues;
+        for (int i : nums)
+        {
+            allValues.insert(i);
+        }
+        for (int i : nums)
+        {
+            if (allValues.contains(i - 1))
+                continue;
+            else
+            {
+                int count = 1;
+                int n = i;
+                while (allValues.contains(n + count))
+                {
+                    count++;
+                }
+                max = max > count ? max : count;
+            }
+        }
+        return max;
+    }
+};
