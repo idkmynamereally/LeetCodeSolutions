63a2638013d7616ffe984b5ba9d79e147216d048
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2023-10-28 12:10:49 +0530

    Time: 83 ms (7.6%), Space: 59.4 MB (15.31%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxArea(std::vector<int>& height) 
+    {
+        int length = 0;
+        int breadth = 0;
+        int maxVolume = -1;
+        int currVolume = -1;
+
+        int l = 0;
+        int r = height.size() - 1;
+
+        while (l < r)
+        {
+            length = r - l;
+            breadth = height[l] < height[r] ? height[l] : height[r];
+            currVolume = length * breadth;
+            maxVolume = maxVolume > currVolume ? maxVolume : currVolume;
+
+            if (height[l] > height[r])
+                r--;
+            else
+                l++;
+        }
+        return maxVolume;
+    }
+};
