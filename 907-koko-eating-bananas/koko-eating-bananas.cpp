429ebc9f695c4f0d65b2744bbd64108f0d48886c
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2023-11-22 18:15:35 +0530

    Time: 41 ms (38.38%) | Memory: 19.3 MB (69.50%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minEatingSpeed(std::vector<int>& piles, int h)
+    {
+        auto maxer{ *std::max_element(piles.begin(), piles.end()) };
+        
+        int left{ 1 };
+        int right{ maxer };
+        int middle{};
+
+        while (left != right)
+        {
+            middle = left + floor((right - left) / 2);
+            auto midWorking = checkK(piles, h, middle);
+
+            if (midWorking)
+                right = middle;
+            else
+                left = middle + 1;
+        }
+
+        return left;
+    }
+
+    bool checkK(std::vector<int>& piles, int h, int k)
+    {
+        for (int p : piles)
+        {
+            h -= ceil((double)p / k);
+            if (h < 0)
+                return false;
+        }
+        return true;
+    }
+};
