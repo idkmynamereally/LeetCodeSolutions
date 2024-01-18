a92edb98508727407108cfafe32d45abd207639e
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-01-18 19:58:40 +0530

    Time: 7 ms (83.78%) | Memory: 22.2 MB (9.52%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -14,13 +14,13 @@ public:
         auto leftTree = isNodeBalanced(node->left);
         auto rightTree = isNodeBalanced(node->right);
 
-        /*int leftH = leftTree.second + 1;
-        int rightH = rightTree.second + 1;*/
+        int leftH = leftTree.second + 1;
+        int rightH = rightTree.second + 1;
 
-        if (leftTree.first && rightTree.first && abs(rightTree.second + 1 - (leftTree.second + 1)) <= 1)
-            return std::pair<bool, int>(true, max(leftTree.second + 1, rightTree.second + 1));
+        if (leftTree.first && rightTree.first && abs(rightH - leftH) <= 1)
+            return std::pair<bool, int>(true, max(leftH, rightH));
         else
-            return std::pair<bool, int>(false, max(leftTree.second + 1, rightTree.second + 1));
-        return std::pair<bool, int>(false, max(leftTree.second + 1, rightTree.second + 1));
+            return std::pair<bool, int>(false, max(leftH, rightH));
+        return std::pair<bool, int>(false, max(leftH, rightH));
     }
 };
