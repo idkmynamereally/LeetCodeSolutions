948232b8765c9af43692644283b7345ca170de7f
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-01-23 03:10:08 +0530

    Time: 89 ms (80.04%) | Memory: 64.4 MB (5.12%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,3 +1,13 @@
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+//speedup i/o
+#define speedIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int init=[]{
+    speedIO;
+    return 0;
+}();
 class Solution
 {
 public:
