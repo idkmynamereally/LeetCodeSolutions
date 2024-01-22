3496d3788921ecbb74fc3f77ebe011f48a8834db
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-01-22 23:07:38 +0530

    Time: 139 ms (95.78%) | Memory: 71 MB (52.29%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,35 +1,39 @@
 class fartherFromOrigin
 {
 public:
-    bool operator()(std::vector<int>& a, std::vector<int>& b)
+    bool operator()(std::pair<int, double>& a, std::pair<int, double>& b)
     {
-        double distanceA = sqrt((double)a[0] * a[0] + a[1] * a[1]);
-        double distanceB = sqrt((double)b[0] * b[0] + b[1] * b[1]);
-        return distanceA < distanceB;
+        return a.second < b.second;
     }
 };
 
-class Solution 
+class Solution
 {
 public:
 
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) 
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
     {
-        std::priority_queue<std::vector<int>, std::vector<std::vector<int>>, fartherFromOrigin> maxHeap;
-        
-        for (const auto& p : points)
-            maxHeap.push(p);
+        std::priority_queue<std::pair<int, double>, std::vector<std::pair<int, double>>, fartherFromOrigin> maxHeap;
+
+        for (int i = 0; i < points.size(); i++)
+        {
+            double distance = sqrt(pow(points[i][0], 2) + pow(points[i][1], 2));
+            maxHeap.push(std::pair<int, double> (i, distance));
+        }
 
         while (maxHeap.size() > k)
+        {
             maxHeap.pop();
-
-        points.clear();
+        }
+        
+        std::vector<std::vector<int>> ans;
 
         while (!maxHeap.empty())
         {
-            points.push_back(maxHeap.top());
+            ans.push_back(points[maxHeap.top().first]);
             maxHeap.pop();
         }
-        return points;
+
+        return ans;
     }
 };
