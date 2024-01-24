a7f021e0294701f4f5a838adac07a3359a890b12
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-01-24 15:51:15 +0530

    Time: 4 ms (27.92%) | Memory: 8.7 MB (8.82%) - LeetSync

--- 355-design-twitter/design-twitter.cpp
+++ 355-design-twitter/design-twitter.cpp
@@ -4,30 +4,28 @@ public:
     std::unordered_map<int, std::unordered_set<int>> users;     //<userId, <useridsfollowed>>
     std::vector<std::pair<int, int>> tweets;        //<userId, TweetId>
     Twitter() 
-    {}
+    {
+        tweets.reserve(10);
+    }
 
     void postTweet(int userId, int tweetId) 
     {
-        if (!users.contains(userId))
-        {
-            users[userId].insert(userId);
-        }
         tweets.push_back(std::pair<int, int>(userId, tweetId));
     }
 
     vector<int> getNewsFeed(int userId) 
     {
-        std::vector<int> ans;
         int count = 0;
+        std::vector<int> ans;
         for (int i = tweets.size() - 1; i >= 0; i--)
         {
             if (count == 10)
                 break;
             auto tweet = tweets[i];
-            if (users[userId].contains(tweet.first))
+            if (users[userId].contains(tweet.first) || tweet.first == userId)
             {
-                count++;
                 ans.push_back(tweet.second);
+                count++;
             }
         }
         return ans;
@@ -35,27 +33,11 @@ public:
 
     void follow(int followerId, int followeeId) 
     {
-        if (!users.contains(followerId))
-        {
-            users[followerId].insert(followerId);
-        }
-        if (!users.contains(followeeId))
-        {
-            users[followeeId].insert(followeeId);
-        }
         users[followerId].insert(followeeId);
     }
 
     void unfollow(int followerId, int followeeId) 
     {
-        if (!users.contains(followerId))
-        {
-            users[followerId].insert(followerId);
-        }
-        if (!users.contains(followeeId))
-        {
-            users[followeeId].insert(followeeId);
-        }
         users[followerId].erase(followeeId);
     }
 };
