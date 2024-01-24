f99d593ffe273ae845f8a75681b7b0512f1db51b
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-01-24 17:26:34 +0530

    Time: 1811 ms (5.09%) | Memory: 678.5 MB (5.09%) - LeetSync

---
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+public:
+    bool isPseudoPalindromic(std::unordered_map<int, int>& map, int count)
+    {
+        bool oddOccurer{ false };
+        if (count % 2 == 0)
+            oddOccurer = true;
+        for (const auto& i : map)
+        {
+            if (i.second % 2 != 0)
+            {
+                if (oddOccurer)
+                    return false;
+                oddOccurer = 1;
+            }
+        }
+        return true;
+    }
+
+    int pseudoPalindromicPaths(TreeNode* root)
+    {
+        int ans{ 0 };
+        std::unordered_map<int, int> map;
+        recurse(root, map, 0, ans);
+        return ans;
+    }
+
+    void recurse(TreeNode* root, std::unordered_map<int, int> map, int count, int& ans)
+    {
+        if (!root)
+            return;
+        
+        count++;
+        map[root->val]++;
+
+        if (!root->left && !root->right)
+        {
+            if (isPseudoPalindromic(map, count))
+                ans++;
+        }
+        if (root->left)
+            recurse(root->left, map, count, ans);
+        if (root->right)
+            recurse(root->right, map, count, ans);
+    }
+};
