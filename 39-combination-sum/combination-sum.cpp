a10c954b39eabe15b3458e88b1a0b957056e2cff
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-01-31 16:37:08 +0530

    Time: 3 ms (85.84%) | Memory: 16.1 MB (37.78%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,5 +1,4 @@
-class Solution 
-{
+class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
     {
@@ -8,20 +7,23 @@ public:
         helper(ans, candidates, curr, 0, target, 0);
         return ans;
     }
-
-    void helper(vector<vector<int>>& ans, vector<int>& nums, vector<int>& curr, int lastAddedIndex, int target, int currSum)
+    
+    void helper(vector<vector<int>>& ans, vector<int>& nums, vector<int>& curr, int currIndex, int target, int currSum)
     {
+        if (currSum > target || currIndex >= nums.size())
+        {
+            return;
+        }
         if (currSum == target)
         {
             ans.push_back(curr);
             return;
         }
-        if (lastAddedIndex == nums.size() || currSum > target)
-            return;
 
-        curr.push_back(nums[lastAddedIndex]);
-        helper(ans, nums, curr, lastAddedIndex, target, currSum + nums[lastAddedIndex]);
+        helper(ans, nums, curr, currIndex + 1, target, currSum);
+
+        curr.push_back(nums[currIndex]);
+        helper(ans, nums, curr, currIndex, target, currSum + nums[currIndex]);
         curr.pop_back();
-        helper(ans, nums, curr, lastAddedIndex + 1, target, currSum);
     }
 };
