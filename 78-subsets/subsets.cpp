1ead4fb653b10ac56175cabf63284568a5899b64
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-01-31 16:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (46.77%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,25 +1,24 @@
-class Solution 
-{
+class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums)
+    vector<vector<int>> subsets(vector<int>& nums) 
     {
-        std::vector<std::vector<int>> ans;
-        std::vector<int> curr;
+        vector<vector<int>> ans;
+        vector<int> curr;
         helper(ans, curr, nums, 0);
         return ans;
     }
 
-    void helper(std::vector<std::vector<int>>& ans, std::vector<int>& curr, std::vector<int>& nums, int indexOfNextItem)
+    void helper(vector<vector<int>>& ans, vector<int>& curr, vector<int>& nums, int currIndex)
     {
-        if (indexOfNextItem == nums.size())
+        if (currIndex == nums.size())
         {
             ans.push_back(curr);
             return;
         }
 
-        helper(ans, curr, nums, indexOfNextItem + 1);
-        curr.push_back(nums[indexOfNextItem]);
-        helper(ans, curr, nums, indexOfNextItem + 1);
+        helper(ans, curr, nums, currIndex + 1);
+        curr.push_back(nums[currIndex]);
+        helper(ans, curr, nums, currIndex + 1);
         curr.pop_back();
     }
 };
