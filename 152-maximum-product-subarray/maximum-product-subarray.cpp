d18596e6a429fc996ba7f0bf8561e16aef1230fe
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-02-09 13:46:01 +0530

    Time: 7 ms (41.52%) | Memory: 16.8 MB (17.12%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -3,20 +3,33 @@ class Solution
 public:
     int maxProduct(vector<int>& nums) 
     {
-        int ans = INT_MIN;
-        for (int i = 0; i < nums.size(); i++)
+        vector<int> maxMul(nums.size(), INT_MIN);
+        vector<int> minMul(nums.size(), INT_MAX);
+
+        maxMul[0] = nums[0];
+        minMul[0] = nums[0];
+
+        for (int i = 1; i < nums.size(); i++)
         {
-            if (nums[i] == 0)
-            {
-                ans = (0 > ans ? 0 : ans);
-                continue;
-            }
-            int curr = 1;
-            for (int j = i; j < nums.size(); j++)
-            {
-                curr *= nums[j];
-                ans = (curr > ans ? curr : ans);
-            }
+            int curr = nums[i];
+            int lastMin = minMul[i - 1];
+            int lastMax = maxMul[i - 1];
+
+            int c1 = lastMin * curr;
+            int c2 = lastMax * curr;
+            
+            minMul[i] = (c1 < c2 ? c1 : c2);
+            minMul[i] = curr < minMul[i] ? curr : minMul[i];
+
+            maxMul[i] = (c1 > c2 ? c1 : c2);
+            maxMul[i] = curr > maxMul[i] ? curr : maxMul[i];
+        }
+
+        int ans = INT_MIN;
+        for (int i : maxMul)
+        {
+            if (i > ans)
+                ans = i;
         }
         return ans;
     }
