70e9636f83934bed042ea521e4b135b4937a73cf
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-02-18 17:57:24 +0530

    Time: 28 ms (80.99%) | Memory: 16.1 MB (56.25%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -3,32 +3,26 @@ class Solution
 public:
     int coinChange(vector<int>& coins, int amount)
     {
-        unordered_map<int, int> dp;
-        int ans = recurse(coins, amount, dp);
-        ans = (ans == INT_MAX) ? -1 : ans;
-        return ans;
-    }
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
 
-    int recurse(vector<int>& nums, int target, unordered_map<int, int>& dp)
-    {
-        if (dp.contains(target))
-            return dp[target];
-        if (target == 0)
-            return 0;
-        if (target < 0)
-            return INT_MAX;
-
-        int min = INT_MAX;
-        for (int i = 0; i < nums.size(); i++)
+        for (int i = 1; i <= amount; i++)
         {
-            int ans = recurse(nums, target - nums[i], dp);
-            if (ans != INT_MAX)
+            int ans = INT_MAX;
+            for (int coin : coins)
             {
-                ans++;
-                min = min < ans ? min : ans;
+                if (i - coin < 0)
+                    continue;
+                if (dp[i - coin] == INT_MAX)
+                    continue;
+                ans = dp[i - coin] + 1;
+                dp[i] = ans < dp[i] ? ans : dp[i];
             }
         }
-        dp[target] = min;
-        return min;
+        
+        if (dp[amount] == INT_MAX)
+            return -1;
+
+        return dp[amount];
     }
 };
