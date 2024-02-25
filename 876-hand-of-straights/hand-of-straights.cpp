b0085597cc809b099a8dcb160411a2936a28b629
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-02-26 01:10:40 +0530

    Time: 73 ms (31.13%) | Memory: 31.6 MB (23.49%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,10 +1,6 @@
 class Solution
 {
 public:
-    Solution()
-    {
-        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-    }
     bool isNStraightHand(vector<int>& hand, int groupSize)
     {
         int size = hand.size();
@@ -18,21 +14,16 @@ public:
         }
         for (int i = 0; i < hand.size(); i++)
         {
-            //std::cout << hand[i] << "\n";
             if (m[hand[i]])
             {
-                int card = hand[i];
-                for (int count = 0; count < groupSize; count++)
+                for (int card = hand[i]; (card - hand[i]) < groupSize; card++)
                 {
                     if (m[card] == 0)
                     {
                         return false;
                     }
-                    //std::cout << card << ", ";
                     m[card]--;
-                    card++;
                 }
-                //std::cout << "\n";
             }
         }
         return true;
