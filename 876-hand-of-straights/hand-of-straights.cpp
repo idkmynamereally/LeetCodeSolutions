f91c434eceed660e3f13aa06e6495318c533245d
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-02-26 01:01:28 +0530

    Time: 63 ms (58.44%) | Memory: 31.6 MB (21.67%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize)
+    {
+        int size = hand.size();
+        if ((size % groupSize) != 0)
+            return false;
+        std::sort(hand.begin(), hand.end());
+        std::map<int, int> m;
+        for (int i : hand)
+        {
+            m[i]++;
+        }
+        for (int i = 0; i < hand.size(); i++)
+        {
+            //std::cout << hand[i] << "\n";
+            if (m[hand[i]])
+            {
+                int card = hand[i];
+                for (int count = 0; count < groupSize; count++)
+                {
+                    if (m[card] == 0)
+                    {
+                        return false;
+                    }
+                    //std::cout << card << ", ";
+                    m[card]--;
+                    card++;
+                }
+                //std::cout << "\n";
+            }
+        }
+        return true;
+    }
+};
