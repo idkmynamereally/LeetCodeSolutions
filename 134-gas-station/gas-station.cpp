beefbd6f65294ebfa18f48a1c0bedf471b41e687
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-02-25 14:28:42 +0530

    Time: 115 ms (37.15%) | Memory: 133.4 MB (5.90%) - LeetSync

---
+++ 134-gas-station/gas-station.cpp
@@ -0,0 +1,54 @@
+class Solution
+{
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
+    {
+        vector<int> affect(gas.size(), 0);
+        for (int i = 0; i < affect.size(); i++)
+        {
+            affect[i] = gas[i] - cost[i];
+        }
+        int l = 0;
+        int r = 0;
+        int ans = 0;
+        int sum = 0;
+
+        while (l < gas.size())
+        {
+            slidingWindow(l, r, affect, sum);
+            if ((r == l) && sum >= 0)
+                return l;
+            if (r <= l)
+                return -1;
+            l = r;
+        }
+
+        return ans;
+    }
+    void slidingWindow(int& l, int& r, vector<int>& affect, int& sum)
+    {
+        sum = affect[l];
+        int size = affect.size();
+        r = (l + 1) % size;
+
+        while (sum >= 0)
+        {
+            if (r == l)
+            {
+                return;
+            }
+            sum += affect[r];
+            r = (r + 1) % size;
+        }
+    }
+    void printWindow(int l, int r, vector<int>& affect)
+    {
+        int size = affect.size();
+        cout << "Window : ";
+        for (int i = l; i != r; i++, i = i % size)
+        {
+            cout << affect[i] << " (" << i << ")" << ", ";
+        }
+        cout << affect[r] << " (" << r << ")" << ", " << "\n";
+    }
+};
