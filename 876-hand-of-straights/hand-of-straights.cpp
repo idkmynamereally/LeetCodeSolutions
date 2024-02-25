f25f4ec4701ba88f632f6a1f66e9e4dc70d2e85d
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-02-26 01:12:52 +0530

    Time: 40 ms (92.01%) | Memory: 30.5 MB (47.23%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -3,11 +3,12 @@ class Solution
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize)
     {
+        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
         int size = hand.size();
         if ((size % groupSize) != 0)
             return false;
         std::sort(hand.begin(), hand.end());
-        std::map<int, int> m;
+        std::unordered_map<int, int> m;
         for (int i : hand)
         {
             m[i]++;
@@ -16,6 +17,7 @@ public:
         {
             if (m[hand[i]])
             {
+                int card = hand[i];
                 for (int card = hand[i]; (card - hand[i]) < groupSize; card++)
                 {
                     if (m[card] == 0)
