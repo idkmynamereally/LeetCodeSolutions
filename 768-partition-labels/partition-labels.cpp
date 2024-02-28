bb2a80f48f939beef0479aeb09464a0b00d2d761
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-02-29 01:46:05 +0530

    Time: 2 ms (78.19%) | Memory: 7.7 MB (64.26%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -3,46 +3,27 @@ class Solution
 public:
     vector<int> partitionLabels(string s)
     {
-        std::pair<int, int> p(-1, -1);
-        vector<pair<int, int>> map(26, p);
         vector<int> ans;
-        
+        vector<int> m(26, -1);
+
         for (int i = 0; i < s.size(); i++)
         {
             char c = s[i];
-            if (map[c - 'a'].first == -1)
-                map[c - 'a'].first = i;
-            map[c - 'a'].second = i;
+            m[c - 'a'] = i;
         }
-        std::sort(map.begin(), map.end(), [](pair<int, int> a, pair<int, int> b)
-            {
-                return(a.first < b.first);
-            });
-        /*for (int i = 0; i < map.size(); i++)
+
+        int end = m[s[0] - 'a'];
+        int prev = -1;
+        for (int i = 0; i < s.size(); i++)
         {
-            cout << map[i].first << " " << map[i].second << "\n";
-        }*/
-        
-        int part = 0;
-        int lastPart = 0;
-        for (int i = 0; i < map.size(); i++)
-        {
-            if (map[i].first == -1)
-                continue;
-            if (map[i].first > part)
+            char c = s[i];
+            end = end > m[c - 'a'] ? end : m[c - 'a'];
+            if (i == end)
             {
-                //cout << part << " -> " << map[i].first << " " << map[i].second << "\n";
-                part++;
-                ans.push_back(part - lastPart);
-                lastPart = part;
-                part = part > map[i].second ? part : map[i].second;
-            }
-            else if(map[i].first <= part)
-            {
-                part = part > map[i].second ? part : map[i].second;
+                ans.push_back(end - prev);
+                prev = end;
             }
         }
-        ans.push_back(part - lastPart + 1);
         return ans;
     }
 };
