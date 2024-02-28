e449aa37b14dd227295d73f9b2397454c4cdd550
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-02-28 11:28:38 +0530

    Time: 432 ms (21.00%) | Memory: 178.3 MB (5.02%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
@@ -9,15 +9,15 @@ public:
         bool p1 = false;
         bool p2 = false;
         bool p3 = false;
-        for (int i = 0; i < triplets.size(); i++)
+        for (const auto& t : triplets)
         {
-            if (triplets[i][0] > target[0] || triplets[i][1] > target[1] || triplets[i][2] > target[2])
+            if (t[0] > target[0] || t[1] > target[1] || t[2] > target[2])
                 continue;
-            if (triplets[i][0] == target[0])
+            if (t[0] == target[0])
                 p1 = true;
-            if (triplets[i][1] == target[1])
+            if (t[1] == target[1])
                 p2 = true;
-            if (triplets[i][2] == target[2])
+            if (t[2] == target[2])
                 p3 = true;
             if (p1 && p2 && p3)
                 return true;
