cfc1cf212b63064c93f3f2841e75538838ca8db7
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-03-26 22:38:02 +0530

    Time: 28 ms (62.38%) | Memory: 15.7 MB (94.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -6,35 +6,31 @@ public:
         int ans = 0;
         int m = grid.size();
         int n = grid[0].size();
-        vector<bool> col(n, false);
-        vector<vector<bool>> visited(grid.size(), col);
         
         for (int i = 0; i < m; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                if (!visited[i][j] && grid[i][j] == '1')
+                if (grid[i][j] == '1')
                 {
                     ans++;
-                    dfs(grid, visited, i, j);
+                    dfs(grid, i, j);
                 }
             }
         }
         return ans;
     }
 
-    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int r, int c)
+    void dfs(vector<vector<char>>& grid, int r, int c)
     {
         if (r == -1 || c == -1 || r >= grid.size() || c >= grid[0].size())
             return;
-        if (visited[r][c] == true)
-            return;
         if (grid[r][c] == '0')
             return;
-        visited[r][c] = true;
-        dfs(grid, visited, r + 1, c);
-        dfs(grid, visited, r, c + 1);
-        dfs(grid, visited, r - 1, c);
-        dfs(grid, visited, r, c - 1);
+        grid[r][c] = '0';
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c - 1);
     }
 };
