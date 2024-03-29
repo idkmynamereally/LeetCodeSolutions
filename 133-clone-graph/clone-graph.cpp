ff87f05830f2697f580214198e9941ed76be034d
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-03-29 11:34:33 +0530

    Time: 4 ms (40.23%) | Memory: 11.9 MB (10.51%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,32 @@
+class Solution 
+{
+public:
+    Node* cloneGraph(Node* node) 
+    {
+        if (node == nullptr)
+            return nullptr;
+
+        unordered_map<Node*, Node*> map;
+        unordered_set<Node*> visited;
+        dfs(node, visited, map);
+        return map[node];
+    }
+
+    void dfs(Node* node, unordered_set<Node*>& visited, unordered_map<Node*, Node*>& map)
+    {
+        if (visited.contains(node))
+            return;
+        
+        visited.insert(node);
+
+        if (!map.contains(node))
+            map[node] = new Node(node->val);
+        Node* currNode = map[node];
+
+        for (auto n : node->neighbors)
+        {
+            dfs(n, visited, map);
+            currNode->neighbors.push_back(map[n]);
+        }
+    }
+};
