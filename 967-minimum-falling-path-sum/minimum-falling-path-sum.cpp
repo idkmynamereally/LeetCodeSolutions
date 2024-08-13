5db58d41d71dc351d6fcc1e08f4a64303fcc005a
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-08-13 22:57:54 +0530

    Time: 12 ms (60.03%) | Memory: 13.2 MB (86.53%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -3,36 +3,26 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix)
     {
         int size = matrix.size();
-        vector<vector<bool>> visited(size, vector<bool>(size, false));
-        for (int j = 0; j < matrix[0].size(); j++)
-            visited[0][j] = true;
-
-        int ans = INT_MAX;
-        for (int j = 0; j < matrix[0].size(); j++)
+        for (int i = 1; i < matrix.size(); i++)
         {
-            ans = min(ans, recurse(matrix, matrix.size() - 1, j, visited));
+            for (int j = 0; j < matrix.size(); j++)
+            {
+                int ans = INT_MAX;
+                ans = min(ans, matrix[i][j] + matrix[i - 1][j]);
+                if (j - 1 >= 0)
+                    ans = min(ans, matrix[i][j] + matrix[i - 1][j - 1]);
+                if (j + 1 < matrix.size())
+                    ans = min(ans, matrix[i][j] + matrix[i - 1][j + 1]);
+
+                matrix[i][j] = ans;
+            }
+        }
+        int ans = INT_MAX;
+        for (int j = 0; j < matrix.size(); j++){
+            ans = min(ans, matrix[size - 1][j]);
         }
 
         return ans;
     }
-
-    int recurse(vector<vector<int>>& mat, int i, int j, vector<vector<bool>>& visited)
-    {
-        if (i >= mat.size() || i < 0 || j >= mat[0].size() || j < 0)
-            return INT_MAX;
-
-        if (visited[i][j])
-            return mat[i][j];
-
-        int minUp = INT_MAX;
-        
-        minUp = min(minUp, recurse(mat, i - 1, j, visited));
-        minUp = min(minUp, recurse(mat, i - 1, j - 1, visited));
-        minUp = min(minUp, recurse(mat, i - 1, j + 1, visited));
-
-        visited[i][j] = true;
-        mat[i][j] = minUp + mat[i][j];
-
-        return mat[i][j];
-    }
 };
+// i-1, j | i-1, j-1 | i-1, j+1
