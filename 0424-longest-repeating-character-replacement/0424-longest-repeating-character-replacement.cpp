42f6d3745e4158c28a26d74c09752f0ec35a0c50
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-08-17 16:14:15 +0530

    Time: 10 ms (46.59%), Space: 9.7 MB (24.91%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int characterReplacement(std::string s, int k) 
+    {
+        int l{ 0 };
+        int r{ 0 };
+        int maxSize{ 0 };
+        int maxf{ 1 };
+        std::unordered_map<char, int> m{};
+        m[s[r]]++;
+        
+        while (r < s.size())
+        {
+            if (isStringValid(r-l+1, maxf, k))
+            {
+                maxSize = maxSize > (r - l + 1) ? maxSize : (r - l + 1);
+                r++;
+                m[s[r]]++;
+                maxf = maxf > m[s[r]] ? maxf : m[s[r]];
+            }
+            else
+            {
+                m[s[l]]--;
+                l++;
+            }
+        }
+        return maxSize;
+    }
+    
+    bool isStringValid(int subStrSize, int highestCharCount, int k)
+    {
+        if (subStrSize - highestCharCount <= k)
+            return true;
+        else
+            return false;
+    }
+};
