bcdc34a25ede62b34e7ad7f6f4d7060ed4d510fb
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-08-25 20:36:08 +0530

    Time: 8 ms (65.11%) | Memory: 14.7 MB (36.30%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -7,24 +7,33 @@ public:
         for (int i = 0; i < nums.size(); i++)
             sum += nums[i];
         int s1 = (target + sum) / 2;
-        
         if (sum % 2 && !(target % 2))
             return 0;
         if (!(sum % 2) && target % 2)
             return 0;
-        return recurse(nums, 0, s1);
+
+        if (s1 < 0)
+            s1 = sum - s1;
+
+        vector<vector<int>> dp(nums.size() + 1, vector<int>(s1 + 1, -1));
+
+        return recurse(nums, 0, s1, dp);
     }
 
-    int recurse(vector<int> &nums, int i, int t)
+    int recurse(vector<int> &nums, int i, int t, vector<vector<int>> &dp)
     {
         if (i == nums.size() && t == 0)
             return 1;
         if (i == nums.size() || t < 0)
             return 0;
 
-        int take = recurse(nums, i + 1, t - nums[i]);
-        int leave = recurse(nums, i + 1, t);
+        if (dp[i][t] != -1)
+            return dp[i][t];
 
-        return take + leave;
+        int take = recurse(nums, i + 1, t - nums[i], dp);
+        int leave = recurse(nums, i + 1, t, dp);
+
+        dp[i][t] = take + leave;
+        return dp[i][t];
     }
 };
