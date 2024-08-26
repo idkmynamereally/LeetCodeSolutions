356d46eb9d105353899929ab8b3ad6b94852445c
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-08-26 21:33:17 +0530

    Time: 26 ms (11.47%) | Memory: 20.1 MB (46.62%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -3,11 +3,37 @@ class Solution
 public:
     int change(int amount, vector<int> &coins)
     {
-        // recursion
+        // ---------recursion---------
         // return recurse(coins, amount, 0);
 
-        vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, -1));
-        return recurse(coins, amount, 0, dp);
+        // --------memoization--------
+        // vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, -1));
+        // return recurse(coins, amount, 0, dp);
+
+        vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, 0));
+
+        for (int i = 0; i <= coins.size(); i++)
+        {
+            for (int t = 0; t <= amount; t++)
+            {
+                if (i == coins.size())
+                    dp[i][t] = 0; // 0 possible subsets if no more coins left
+                if (t == 0)
+                    dp[i][t] = 1; // 1 possible subset if amount to make is 0
+            }
+        }
+
+        for (int i = coins.size() - 1; i >= 0; i--)
+        {
+            for (int t = 1; t <= amount; t++)
+            {
+                if (t - coins[i] >= 0)
+                    dp[i][t] = dp[i][t - coins[i]];
+                dp[i][t] += dp[i + 1][t];
+            }
+        }
+
+        return dp[0][amount];
     }
 
     int recurse(vector<int> &coins, int t, int i, vector<vector<int>> &dp)
