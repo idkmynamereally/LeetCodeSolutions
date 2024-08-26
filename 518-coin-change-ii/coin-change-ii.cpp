16c21d363ca45e27288294c783e9f5674b3fdaa3
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-08-26 21:38:41 +0530

    Time: 21 ms (17.42%) | Memory: 20 MB (46.62%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -12,16 +12,10 @@ public:
 
         vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, 0));
 
+        for (int t = 0; t <= amount; t++)
+            dp[coins.size()][t] = 0; // 0 possible subsets if no more coins left
         for (int i = 0; i <= coins.size(); i++)
-        {
-            for (int t = 0; t <= amount; t++)
-            {
-                if (i == coins.size())
-                    dp[i][t] = 0; // 0 possible subsets if no more coins left
-                if (t == 0)
-                    dp[i][t] = 1; // 1 possible subset if amount to make is 0
-            }
-        }
+            dp[i][0] = 1; // 1 possible subset if amount to make is 0
 
         for (int i = coins.size() - 1; i >= 0; i--)
         {
