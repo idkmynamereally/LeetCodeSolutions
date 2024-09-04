baa424d05608c93e90d86bd53652ae83bed004c1
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-09-04 19:45:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (19.19%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,3 +1,5 @@
+#define sv string_view
+
 class Solution
 {
 public:
@@ -7,50 +9,45 @@ public:
         return recurse(s, p, 0, 0, dp);
     }
 
-    bool recurse(string_view s, string_view p, int i, int j, vector<vector<int>> &dp)
+    bool recurse(sv s, sv p, int i, int j, vector<vector<int>> &dp)
     {
+        bool ans = false;
+
         if (i == s.size() && j == p.size())
             return true;
-        if (i == s.size())
-        {
-            int left = p.size() - j;
-            if (left % 2)
-                return false;
-            j += 1;
-            while (j < p.size() && p[j] == '*')
-                j += 2;
-            if (j < p.size())
-                return false;
-            return true;
-        }
-        if (j >= p.size())
+
+        if (j == p.size())
             return false;
 
         if (dp[i][j] != -1)
             return dp[i][j];
 
-        if (j + 1 != p.size() && p[j + 1] == '*')
+        if (i == s.size())
         {
-            bool ans = false;
-            if (s[i] == p[j] || p[j] == '.')
-            {
-                ans |= recurse(s, p, i + 1, j, dp);
-                ans |= recurse(s, p, i + 1, j + 2, dp);
-            }
-            ans |= recurse(s, p, i, j + 2, dp);
-            dp[i][j] = ans;
-            return ans;
-        }
-        else
-        {
-            if (s[i] == p[j] || p[j] == '.')
-                dp[i][j] = recurse(s, p, i + 1, j + 1,dp);
+            if (j == p.size() - 1)
+                return false;
+            if (p[j + 1] == '*')
+                dp[i][j] = recurse(s, p, i, j + 2, dp);
             else
                 dp[i][j] = false;
-
             return dp[i][j];
         }
-        dp[i][j] = false;
+
+        if (p[j + 1] == '*')
+        {
+            if (s[i] == p[j] || p[j] == '.')
+            {
+                ans |= recurse(s, p, i + 1, j, dp); // More occur of curr char
+            }
+            ans |= recurse(s, p, i, j + 2, dp); // Matched all we can
+        }
+
+        else if (s[i] == p[j] || p[j] == '.')
+        {
+            ans |= recurse(s, p, i + 1, j + 1, dp);
+        }
+
+        dp[i][j] = ans;
         return dp[i][j];
-    };
+    }
 };
