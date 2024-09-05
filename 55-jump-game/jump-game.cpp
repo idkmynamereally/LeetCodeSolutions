cf78219cbdbb5cc6506fa69ca57f7045215f1bee
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-09-05 20:44:15 +0530

    Time: 683 ms (6.18%) | Memory: 51.5 MB (17.29%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,20 +1,21 @@
 class Solution
 {
 public:
-    bool canJump(vector<int>& nums)
+    bool canJump(vector<int> &nums)
     {
-        int closest = nums.size() - 1;
-        makeSet(nums, closest, closest);
-        return (closest == 0);
-    }
+        vector<bool> can(nums.size(), false);
+        can[nums.size() - 1] = true;
 
-    void makeSet(vector<int>& nums, int& closest, int currIndex)
-    {
-        if (currIndex < 0)
-            return;
-        int maxJump = nums[currIndex];
-        if (currIndex + maxJump >= closest)
-            closest = currIndex;
-        makeSet(nums, closest, currIndex - 1);
+        for (int i = nums.size() - 2; i >= 0; i--)
+        {
+            bool ans = false;
+            for (int j = i + 1; !ans && j <= i + nums[i]; j++)
+            {
+                ans |= can[j];
+            }
+            can[i] = ans;
+        }
+
+        return can[0];
     }
 };
