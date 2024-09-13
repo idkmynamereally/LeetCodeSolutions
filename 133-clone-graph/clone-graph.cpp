62bfb18dd34149d0d39466e51ba58318f172ecd8
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-09-13 12:31:46 +0530

    Time: 7 ms (22.96%) | Memory: 11.8 MB (58.89%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -3,23 +3,24 @@ class Solution
 public:
     Node *cloneGraph(Node *node)
     {
-        unordered_map<int, Node *> mp;
-        return clone(node, mp);
+        if (!node)
+            return node;
+        unordered_map<Node *, Node *> mp;
+        mp[nullptr] = nullptr;
+        clone(node, mp);
+        return mp[node];
     }
 
-    Node *clone(Node *n, unordered_map<int, Node *> &mp)
+    void clone(Node *node, unordered_map<Node *, Node *> &mp)
     {
-        if (!n)
-            return nullptr;
-        if (mp.count(n->val))
-            return mp[n->val];
+        mp[node] = new Node(node->val);
+        Node *currNode = mp[node];
 
-        mp[n->val] = new Node(n->val);
-
-        for (const auto& i : n->neighbors)
+        for (const auto &i : node->neighbors)
         {
-            mp[n->val]->neighbors.push_back(clone(i, mp));
+            if (!mp.count(i))
+                clone(i, mp);
+            currNode->neighbors.push_back(mp[i]);
         }
-        return mp[n->val];
     }
 };
