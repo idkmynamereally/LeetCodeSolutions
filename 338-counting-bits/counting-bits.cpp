cc029ee2fdc2a8571f21fba917d83a0e929b3591
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-09-15 13:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (98.71%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -6,19 +6,11 @@ public:
         vector<int> ans(n + 1);
         for (int i = 0; i <= n; i++)
         {
-            ans[i] = countSetBits(i);
-        }
-        return ans;
-    }
-
-    int countSetBits(int n)
-    {
-        int ans = 0;
-        while (n)
-        {
-            if (n & 1)
-                ans++;
-            n >>= 1;
+            if (i == 0)
+                ans[0] = 0;
+            if (i == 1)
+                ans[1] = 1;
+            ans[i] = (i & 1) + ans[i >> 1];
         }
         return ans;
     }
