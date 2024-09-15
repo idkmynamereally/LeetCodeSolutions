958da4cdd33e6c67632ef61213310e8ef7286c12
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-09-15 17:27:40 +0530

    Time: 11 ms (71.42%) | Memory: 22.5 MB (58.84%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -9,32 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-class Solution 
-{
+class Solution {
 public:
-	int diameterOfBinaryTree(TreeNode* root)
-	{
-		if (root == nullptr)
-			return 0;
-		int leftSubtreeHeight = maxDepth(root->left);		//add these two if longest path passes throught current node
-		int rightSubtreeHeight = maxDepth(root->right);
-		int ans = max(diameterOfBinaryTree(root->left), diameterOfBinaryTree(root->right));	//call same function if the longest path passes through childeren
-		ans = max(ans, (leftSubtreeHeight + rightSubtreeHeight));	//if yes then choose the largest of the three.
-		return ans;
-	}
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans = 0;
+        recurse(root, ans);
+        return ans;
+    }
 
-	int maxDepth(TreeNode* root)
-	{
-		return depth(root, 0);
-	}
+    int recurse(TreeNode* root, int& ans)
+    {
+        if (!root)
+            return 0;
+        int leftDepth = recurse(root->left, ans);
+        int rightDepth = recurse(root->right, ans);
 
-	int depth(TreeNode* root, int deep)
-	{
-		if (root == nullptr)
-			return deep;
-		int leftHeight = depth(root->left, deep+1);
-		int rightHeight = depth(root->right, deep+1);
-		return (leftHeight > rightHeight ? leftHeight : rightHeight);
-	}
+        ans = max(ans, leftDepth + rightDepth);
+
+        return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+    }
 };
