3735cd054500d5706eb250818f4d38696bd6aaad
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-09-17 23:18:56 +0530

    Time: 2 ms (43.60%) | Memory: 10.5 MB (50.02%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) 
+    {
+        int i = digits.size() - 1;
+        while (i >= 0 && digits[i] == 9)
+        {
+            digits[i] = 0;
+            i--;
+        }
+        if (i == -1)
+        {
+            digits.push_back(0);
+            digits[0] = 1;
+        }
+        else
+        {
+            digits[i]++;
+        }
+        return digits;
+
+    }
+};
