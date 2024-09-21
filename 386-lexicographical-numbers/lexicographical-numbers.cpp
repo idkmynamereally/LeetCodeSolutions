3e386beeaa06c1c4796cc8b8245b5684e8807170
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-09-21 16:15:42 +0530

    Time: 23 ms (38.72%) | Memory: 13.7 MB (76.44%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    vector<int> lexicalOrder(int n)
+    {
+        vector<int> ans;
+        for (int i = 1; i < 10; i++)
+            dfs(n, i, ans);
+        return ans;
+    }
+
+    void dfs(int n, int num, vector<int> &ans)
+    {
+        if (num <= n)
+            ans.push_back(num);
+        else
+            return;
+        for (int d = 0; d < 10; d++)
+        {
+            num *= 10;
+            num += d;
+            dfs(n, num, ans);
+            num -= d;
+            num /= 10;
+        }
+        return;
+    }
+};
