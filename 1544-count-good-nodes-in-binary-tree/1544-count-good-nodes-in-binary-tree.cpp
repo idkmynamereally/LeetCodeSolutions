ccf39c395b5b186a015af4f1b9ed4c6e72687233
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-09-22 18:06:18 +0530

    Time: 99 ms (48.23%), Space: 86.1 MB (12.78%) - LeetHub

---
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int goodNodes(TreeNode *root)
+    {
+        stack<int> st;
+        int ans = 1;
+        findMax(root, st, ans);
+        return ans;
+    }
+
+    void findMax(TreeNode *node, stack<int> &st, int &ans)
+    {
+        if (!node)
+            return;
+
+        if (st.empty())
+            st.push(node->val);
+        else if (node->val >= st.top())
+        {
+            ans++;
+            st.push(node->val);
+        }
+        findMax(node->left, st, ans);
+        findMax(node->right, st, ans);
+
+        if (st.top() == node->val)
+            st.pop();
+    }
+};
