ade753ed6c2beb84f5b94543270f05eee1f28735
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-09-29 00:47:01 +0530

    Time: 3 ms (99.96%) | Memory: 8.6 MB (99.95%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,3 +1,24 @@
+int speedUp = [] {
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    return 0;
+}();
+int init = [] {
+	cin.tie(nullptr)->sync_with_stdio(false);
+    freopen("user.out", "w", stdout);
+
+    for (string s; getline(cin, s);) {
+        int solution = 0, minPrice = INT_MAX;
+        for (int _i = 1, _n = s.length(); _i < _n; ++_i) {
+            int price = s[_i++] & 15;
+            while ((s[_i] & 15) < 10) price = price * 10 + (s[_i++] & 15);
+            minPrice = min(minPrice, price);
+            solution = max(solution, price - minPrice);
+        }
+    cout << solution << '\n';    }
+    return 0;
+}();
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices)
