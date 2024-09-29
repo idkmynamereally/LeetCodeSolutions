581d3a22246119917c8b0490b83c1fa878452576
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-09-29 21:15:12 +0530

    Time: 7 ms (83.96%) | Memory: 17.4 MB (8.54%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,26 +1,27 @@
-class Solution
-{
+class Solution {
 public:
-    void setZeroes(vector<vector<int>> &matrix)
+    void setZeroes(vector<vector<int>>& matrix)
     {
         unordered_set<int> cols;
         unordered_set<int> rows;
+
         for (int i = 0; i < matrix.size(); i++)
         {
             for (int j = 0; j < matrix[0].size(); j++)
             {
-                if (matrix[i][j] == 0)
+                if (!matrix[i][j])
                 {
-                    rows.insert(i);
                     cols.insert(j);
+                    rows.insert(i);
                 }
             }
         }
+
         for (int i = 0; i < matrix.size(); i++)
         {
             for (int j = 0; j < matrix[0].size(); j++)
             {
-                if (cols.count(j) || rows.count(i))
+                if (cols.contains(j) || rows.contains(i))
                     matrix[i][j] = 0;
             }
         }
