4fcae4408ff61743d8b5922b96330433d33c6e34
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-10-02 18:06:56 +0530

    Time: 20 ms (76.74%) | Memory: 23.1 MB (28.10%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,37 @@
+int speedUp = []
+{
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    return 0;
+}();
+
+class Solution
+{
+public:
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
+        sort(intervals.begin(), intervals.end(),
+             [](vector<int> &a, vector<int> &b)
+             {
+                return a[0] < b[0];
+             });
+
+        vector<vector<int>> ans;
+        int n = intervals.size();
+        vector<int> curr{intervals[0]};
+
+        for (int i = 1; i < n; i++)
+        {
+            if (intervals[i][0] <= curr[1])
+            {
+                curr[1] = curr[1] > intervals[i][1] ? curr[1] : intervals[i][1];
+                continue;
+            }
+            ans.push_back(curr);
+            curr[0] = intervals[i][0];
+            curr[1] = intervals[i][1];
+        }
+        ans.push_back(curr);
+        return ans;
+    }
+};
