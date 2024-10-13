5f5c512ea7b36632fe573b6549caa4dedc38ccd3
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-10-13 15:58:35 +0530

    Time: 25 ms (67.94%), Space: 30.3 MB (22.12%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int l = 0;
+        int r = nums.size() - 1;
+        int m;
+
+        while (l <= r)
+        {
+            m = (l + r) / 2;
+            if (nums[m] == target)
+                return m;
+            else if (nums[m] > target)
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        return -1;
+    }
+};
