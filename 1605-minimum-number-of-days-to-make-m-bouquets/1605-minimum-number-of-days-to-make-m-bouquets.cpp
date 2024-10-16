2eaef3522061f0f6a548b4e8829fc3cdbce4fb75
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2024-10-16 11:26:35 +0530

    Time: 117 ms (37.98%), Space: 69.3 MB (39.02%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int minDays(vector<int>& bloomDay, int m, int k)
+    {
+        int l = 1;
+        int r = 0;
+
+        for (int i : bloomDay)
+            r = r > i ? r : i;
+
+        int ans = -1;
+        int days;
+
+        while (l <= r)
+        {
+            days = l + floor((r - l) / 2);
+            int boqs = boquets(bloomDay, m, k, days);
+            
+            if (boqs >= m)
+            {
+                ans = days;
+                r = days - 1;
+            }
+            else
+            {
+                l = days + 1;
+            }
+        }
+        return ans;
+    }
+
+    int boquets(vector<int>& arr, int m, int k, int days)
+    {
+        int count = 0;
+        int bouqets = 0;
+
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (arr[i] <= days)
+                count++;
+            else
+                count = 0;
+            
+            if (count == k)
+            {
+                bouqets++;
+                count = 0;
+            }
+        }
+        return bouqets;
+    }
+};
