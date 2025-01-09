ed42f5cebdd55ee101fbee2869606a3810ee9a2e
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2025-01-10 01:12:39 +0530

    Time: 10 ms (19.49%) | Memory: 15.2 MB (25.15%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -1,24 +1,25 @@
-class Solution {
+class Solution
+{
 public:
-    Node* copyRandomList(Node* head) 
+    Node *copyRandomList(Node *head)
     {
-        std::unordered_map<Node*, Node*> m;
-        Node* temp = head;
+        unordered_map<Node *, Node *> m;
+
+        Node *temp = head;
+
         while (temp)
         {
             m[temp] = new Node(temp->val);
             temp = temp->next;
         }
-        
         temp = head;
-        Node* newTemp = m[head];
         while (temp)
         {
-            newTemp->next = m[temp->next];
-            newTemp->random = m[temp->random];
+            m[temp]->next = m[temp->next];
+            m[temp]->random = m[temp->random];
             temp = temp->next;
-            newTemp = newTemp->next;
         }
+
         return m[head];
     }
 };
