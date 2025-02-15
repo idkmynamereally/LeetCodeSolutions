fea385f4e20dd04897857ae6750f0a7a58e81c57
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2025-02-16 00:13:53 +0530

    Time: 96 ms (28.93%) | Memory: 87.7 MB (29.46%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,32 +1,26 @@
-class Solution
-{
+class Solution {
 public:
-    int longestConsecutive(vector<int> &nums)
-    {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
+    int longestConsecutive(vector<int>& nums) {
         unordered_set<int> set;
-        for (int i : nums)
-            set.insert(i);
-
         int ans = 0;
-
-        for (int i = 0; i < nums.size(); i++)
+        for (int i : nums)
         {
-            int head = nums[i];
-            while (set.count(head))
+            set.insert(i);
+        }
+
+        for (int i : nums)
+        {
+            int a = 0;
+            while (set.contains(i))
+                i--;
+            i++;
+            while (set.contains(i))
             {
-                head--;
+                a++;
+                set.erase(i);
+                i++;
             }
-            head++;
-            int t = 0;
-            while (set.count(head))
-            {
-                set.erase(head);
-                head++;
-                t++;
-            }
-            ans = ans > t ? ans : t;
+            ans = max(ans, a);
         }
         return ans;
     }
