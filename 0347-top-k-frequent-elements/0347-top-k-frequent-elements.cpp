73bae65301b21b5a9c82b063c22a88447e00b0af
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2025-02-16 00:49:50 +0530

    Time: 0 ms (100%), Space: 18 MB (47.98%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,37 +1,29 @@
-class Solution 
-{
+class Solution {
 public:
-    std::vector<int> topKFrequent(std::vector<int>& nums, int k)
-    {
-        std::vector<int> ans(k);
-        std::map<int, int> freq;
-
-        for (const auto& i : nums)  //Got frequency of all numbers...
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        struct custom
         {
-            freq[i]++;
+            bool operator() (const pair<int, int> a, const pair<int, int> b){
+                return a.second < b.second;
+            }
+        };
+
+        vector<int> ans;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, custom> p;
+
+        unordered_map<int, int> mp;
+        for (int i : nums)
+            mp[i]++;
+
+        for (auto it : mp)
+            p.push(it);
+
+        while (k--)
+        {
+            ans.push_back(p.top().first);
+            p.pop();
         }
 
-        std::vector<std::pair<int, int>> sorter(std::size(freq));
-
-        for (const auto& p : freq)
-        {
-            sorter.push_back(p);
-        }
-
-        std::sort(sorter.begin(), sorter.end(),
-            [](const std::pair<int, int>& a, const std::pair<int, int>& b)
-            {
-                return a.second > b.second;
-            });
-        //Sort vector on basis of frequency.
-        int count = 0;
-        for (const auto& i : sorter)    //From the vector sorted in descending on frequency we get the first k elements
-        {
-            ans[count] = (i.first);
-            count++;
-            if (count == k)
-                break;
-        }
         return ans;
     }
 };
