969c79e10fa1f60e1fafe4ea8b8572f6a4b3e52c
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2025-02-17 00:28:05 +0530

    Time: 774 ms (7.73%) | Memory: 82.5 MB (67.21%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -1,58 +1,34 @@
-class Solution 
-{
+class Solution {
 public:
-    bool fleet(std::pair<int, int> behind, std::pair<int, int> ahead, int target)
-    {
-        double t1 = (target - behind.first) / (double)behind.second;
-        double t2 = (target - ahead.first) / (double)ahead.second;
-        if (t1 <= t2)
-            return true;
-        return false;
-    }
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> cars;
 
-    int carFleet(int target, std::vector<int>& position, std::vector<int>& speed)
-    {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        if (position.size() == 1)
-            return 1;
-        
-        std::vector < std::pair<int,int> > posSpeed(position.size());
-        for (int i = 0; i < position.size(); i++)
+        for (int i = 0; i < speed.size(); i++)
         {
-            posSpeed[i] = std::pair(position[i], speed[i]);
+            cars.push_back({position[i], speed[i]});
         }
-        std::sort(posSpeed.begin(), posSpeed.end(),
-            [](std::pair<int,int> a, std::pair<int, int> b)
-                  {
-                      return a.first < b.first;
-                  }
-        );
-        std::stack<std::pair<int, int>> st;
-        int ans{ 1 };
-        for (const auto& i : posSpeed)
-        {
-            st.push(i);
-        }
-        while (!st.empty())
-        {
-            auto top = st.top();
-            st.pop();
-            while (!st.empty())
+
+        sort(cars.begin(), cars.end(), 
+            [](const pair<int, int> a, const pair<int, int> b)
             {
-                auto behind = st.top();
+                return a < b;
+            });
+        
+        stack<float> st;
+        for (int i = 0; i < cars.size(); i++)
+        {
+            int d = target - cars[i].first;
+            float t = (float) d / cars[i].second;
+
+            cout << t << " ";
+
+            while (!st.empty() && st.top() <= t)
+            {
                 st.pop();
-                if (fleet(behind, top, target))
-                {
-                    continue;
-                }
-                else
-                {
-                    ans++;
-                    top = behind;
-                }
             }
+            st.push(t);
         }
-        return ans;
+
+        return st.size();
     }
 };
