66904b9865ff433ff8fa2deb8eb9eb4e7e6c7e74
Harshit Saraswat <97702836+idkmynamereally@users.noreply.github.com>
2025-02-18 21:59:59 +0530

    Time: 49 ms (5.18%) | Memory: 111.8 MB (16.33%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temp) {
+        ios_base::sync_with_stdio(0),cin.tie(0);
         stack<pair<int, int>> st;
         vector<int> ans(temp.size(), 0);
         for (int i = 0; i < temp.size(); i++)
